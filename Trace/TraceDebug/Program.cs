f8c316f94d81b643e8cc2dd26318cb90b317ecfe
subramanian <subramanian.nk@live.com>
2022-10-23 22:38:16 -0500

    Debug and Trace static class working. Maybe for web applications console is the default. Here console is added to tracelistener

---
+++ Trace/TraceDebug/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace TraceDebug
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var listener = new ConsoleTraceListener();
+            Trace.Listeners.Add(listener);
+            Trace.WriteLine("I am in the main method", "tracing");
+            Debug.WriteLine("I am in the main method", "Debug");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
