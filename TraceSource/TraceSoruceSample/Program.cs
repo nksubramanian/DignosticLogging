8cf3e6adcd35cf3fd7908a721e612d551be46267
subramanian <subramanian.nk@live.com>
2022-10-23 23:47:52 -0500

    Don't know why xml traceoutput is not working

---
+++ TraceSource/TraceSoruceSample/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TraceSoruceSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TraceSourceExample traceSourceExample = new TraceSourceExample();
+            traceSourceExample.moreControl();
+            var result = traceSourceExample.performAddition("2", "3");
+            traceSourceExample.RunSample();
+            traceSourceExample.performAddition("s", "1");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ TraceSource/TraceSoruceSample/TraceSourceExample.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraceSoruceSample
+{
+    public class TraceSourceExample
+    {
+        static TraceSource ts = new TraceSource("TraceSourceExample");
+
+        public TraceSourceExample()
+        {
+            SourceSwitch sourceSwitch = new SourceSwitch("mySwitch");
+            sourceSwitch.Level = SourceLevels.Verbose;
+            ts.Switch = sourceSwitch;
+            var listener = new ConsoleTraceListener();
+            int tsConsole = ts.Listeners.Add(listener);
+            ts.Listeners[tsConsole].Name = "Console";
+            XmlWriterTraceListener xmlWriterTraceListener = new XmlWriterTraceListener(
+                Path.Combine(Environment.CurrentDirectory, "xmlTraceListenerOutput.xml"));
+            int tsXml = ts.Listeners.Add(xmlWriterTraceListener);
+            ts.Listeners[tsXml].Name = "Xml";
+
+        }
+
+        public void moreControl()
+        {
+            ts.Listeners["Console"].TraceOutputOptions = TraceOptions.Timestamp | TraceOptions.Callstack;
+     
+        }
+
+        public void RunSample()
+        {
+            ts.TraceEvent(TraceEventType.Verbose, 0, "In RunSample method");
+            ts.TraceEvent(TraceEventType.Verbose, 0, "I am in RunSample method");
+        }
+
+        public int performAddition(string a, string b)
+        {
+            
+            try
+            {
+                return int.Parse(a) + int.Parse(b);
+            }
+            catch(Exception ex)
+            {
+                string errorContext = a + b;
+                ts.TraceData(TraceEventType.Error, 3, new object[] { errorContext, ex });
+                return 0;
+            }
+        }
+
+
+    }
+}
