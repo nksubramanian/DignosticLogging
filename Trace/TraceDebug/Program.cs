7c45ed9d3b629e8ed203de7ed5edbb9db2478679
subramanian <subramanian.nk@live.com>
2022-10-23 22:48:41 -0500

    Debug assertion completed

--- Trace/TraceDebug/Program.cs
+++ Trace/TraceDebug/Program.cs
@@ -11,6 +11,31 @@ namespace TraceDebug
             Trace.WriteLine("I am in the main method", "tracing");
             Debug.WriteLine("I am in the main method", "Debug");
             Console.WriteLine("Hello, World!");
+            Console.WriteLine(findSum(3, 4));
+            seeForAssert();
         }
+
+        private static int findSum(int a, int b)
+        {
+            Debug.WriteLine("I am inside the private method", "tracing");
+            Debug.Indent();
+            Debug.WriteLineIf(5 > 1, "I am printed if true", "I am printed if false");
+            Debug.Unindent();
+            Debug.WriteLine("This statement is solely for seeing the unidenting");
+            return a + b;
+        }
+
+        private static void seeForAssert()
+        {
+            // Create an index for an array.
+            int index;
+
+            // Perform some action that sets the index.
+            index = -40;
+
+            // Test that the index value is valid.
+            Debug.Assert(index > -1);
+        }
+
     }
 }
